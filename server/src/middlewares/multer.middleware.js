53399060a5ec1928ee67177b12611445725e4634
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-04-23 23:48:34 +0530

    Implement file upload with cloudinary Integration,,  error , response, handling utilities

---
+++ server/src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination : function(req, file, cb){
+        cb(null, "./public/");
+    },
+    filename : function(req, file, cb){
+        cb(null, file.filename + "-" + Date.now() + path.extname(file.originalname));
+    }
+})
+
+
+export const upload = multer({
+    storage,
+    limits : { fileSize : 50 * 1024 * 1024 }
+})
---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,67 @@
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config({ path : "./.env" });
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try{
+        if(!localFilePath){
+            throw new Error("No file path provided");
+        }
+
+        const cloudinaryResponse = await cloudinary.uploader.upload(localFilePath, {
+            resource_type : "image",
+            folder : "Plypt"
+        })
+        if(fs.existsSync(localFilePath)){
+            fs.unlinkSync(localFilePath);
+            console.log(`File ${localFilePath} successfully deleted`);
+        }
+        return cloudinaryResponse;
+
+    }catch(err){
+        if(localFilePath && fs.existsSync(localFilePath)){
+            fs.unlinkSync(localFilePath);
+        }
+        console.error(`Cloudinary Upload Error : ${err.message}`);
+        throw new Error("Failed to Upload file to cloudinary")
+    }
+}
+
+const deleteMultipleFromCloudinary = async(publicIds) => {
+    try{
+        await cloudinary.api.delete_resources(publicIds);
+    }catch(err){
+        console.error(`Cloudinary batch delete error : ${err}`);
+        throw new Error("Failed to delete images from cloudinary");
+    }
+}
+
+const deleteFromCloudinary = async(public_id) => {
+    try{
+        await cloudinary.uploader.destroy(
+            public_id, {
+                invalidate : true,
+                resource_type : "image"
+            }
+        )
+    }catch(err){
+        console.log(`Error occurred while deleting file from cloudinary : ${err}`);
+        return null;
+    }
+}
+
+
+export { 
+    uploadOnCloudinary,
+    deleteFromCloudinary,
+    deleteMultipleFromCloudinary
+}
