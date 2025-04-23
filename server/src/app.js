4aecb937246279890e1fc6b15a61b04793f94964
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-04-23 18:01:33 +0530

    Add server configuration and dependencies
    - Implement server setup with Express, CORS, and middleware
    - Update package.json and package-lock.json with new dependencies
    - Create database connection utility
    - Establish Socket.IO server for real-time communication

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+config({ path: "./.env" });
+const app = express();
+
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials: true,
+}))
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(morgan("dev"));
+
+
+export default app;
+
+
---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+import app from "./app.js";
+import http from "http";
+import { connectDB } from "./config/db.js";
+import { Server } from "socket.io";
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors : {
+        origin : "*",
+        methods : ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("New Client connected : " + socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Client Disconnected : " + socket.id);
+    })
+    
+})
+
+connectDB().then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    })
+})
+
+
