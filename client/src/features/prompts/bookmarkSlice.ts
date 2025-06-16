f365921b73e0d8b7bd8285996dd3b04618a6a21b
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-06-16 18:28:21 +0530

    Refactor promptSlice to improve state management for prompts; enhance thunks for better error handling and user feedback. Update store configuration and clean up app.js for improved routing structure.

---
+++ client/src/features/prompts/bookmarkSlice.ts
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import axiosInstance from "../../helpers/axiosInstance";
+import toast from "react-hot-toast";
+import { toastHandler } from "../../helpers/toastHandler";
+
+
+export const toggleBookmarkThunk = createAsyncThunk("bookmark/toggle", async({ promptId } : any, {rejectWithValue}) => {
+    try{
+        const promise = axiosInstance.post(`bookmark/toggle/${promptId}`);
+        toastHandler(promise, "Wait for a moment", "Successfully toggled the bookmark for prompt");
+        const res = await promise;
+        return res.data;
+    }catch(err : any){
+        return rejectWithValue(err?.response?.data?.message || "Error occurred while toggling prompt for bookmark");
+    }
+})
+
+export const fetchMyBookmarksThunk = createAsyncThunk("bookmark/my", async(_, {rejectWithValue}) => {
+    try{
+        const promise = axiosInstance.get("bookmark/my-bookmarks");
+        toastHandler(promise, "Fetching your bookmarks", "Successfully fetched the bookmarks");
+        const res = await promise;
+        return res.data;
+    }catch(err : any){  
+        return rejectWithValue(err?.response?.data?.message || "Error occurred while fething my bookmarks");
+    }
+})
+
+
+const likeSlice = createSlice({
+    name : "bookmark",
+    initialState : {},
+    reducers : {}
+})
+
+export default likeSlice.reducer;
---
+++ client/src/features/prompts/likeSlice.ts
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import axiosInstance from "../../helpers/axiosInstance";
+import toast from "react-hot-toast";
+import { toastHandler } from "../../helpers/toastHandler";
+
+
+export const toggleLikeThunk = createAsyncThunk("like/toggle", async({ promptId } : any, { rejectWithValue }) => {
+    try{
+        const promise = axiosInstance.post(`like/toggle/${promptId}`);
+        toastHandler(promise, "Wait for a moment...", "Successfully toggled the like");
+        const res = await promise;
+        return res.data;
+
+    }catch(err : any){
+        return rejectWithValue(err?.response?.data?.message || "Error occurred while toggling like ");
+    }
+})
+
+export const getPromptLikesThunk = createAsyncThunk("like/count", async({ promptId } : any, {rejectWithValue}) => {
+    try{
+        const promise = axiosInstance.get(`like/count/${promptId}`);
+        toastHandler(promise, "Fetching prompt likes", "Successfully fetched prompts likes");
+        const res = await promise;
+        return res.data;
+    }catch(err : any){
+        return rejectWithValue(err?.response?.data?.message || "Error occurred while fetching prompt likes count");
+    }
+})
+
+
+export const getTopLikedPromptThunk = createAsyncThunk("like/top",async(_, {rejectWithValue}) => {
+    try{
+        const promise = axiosInstance.get(`like/top-liked`);
+        toastHandler(promise, "fetching top liked prompts", "Successfully fetched top liked prompts");
+        const res = await promise;
+        return res.data;
+    }catch(err : any){
+        return rejectWithValue(err?.response?.data?.message || "Error occurred while fetching top liked prompts");
+    }
+})
+
+
+const likeSlice = createSlice({
+    name : "like",
+    initialState : {},
+    reducers : {}
+})
+
+export default likeSlice.reducer;
